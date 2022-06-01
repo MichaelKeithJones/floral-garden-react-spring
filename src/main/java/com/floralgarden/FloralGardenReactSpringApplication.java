9698fc2ad63e957a384f35decdaae469ffa2b872
Michael Jones <michael.k.jones.20@gmail.com>
2022-05-31 22:25:33 -0500

    Spring Boot framework created.

---
+++ src/main/java/com/floralgarden/FloralGardenReactSpringApplication.java
@@ -0,0 +1,14 @@
+package com.floralgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FloralGardenReactSpringApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloralGardenReactSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/floralgarden/FloralGardenReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floralgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloralGardenReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
